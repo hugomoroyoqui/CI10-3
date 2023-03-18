f29313bd50dced3cdf32b8556b1e289833956373
Hugo Moroyoqui <hugo_moroyoqui@hotmail.com>
2023-03-17 18:38:35 -0700

    weather

--- test.js
+++ test.js
@@ -1,3 +1,15 @@
 var v1 = 13, v2 =15;
 
 console.log(v1+v2);
+
+const axios = require('axios');
+
+const fetchWeatherData = async () => {
+    const { data } = await axios.get(
+      'https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=868cc7dbcc306312dfd6a00c57d0fd6a&units=metric'
+    );
+    console.log(data);
+};
+
+fetchWeatherData();
+
