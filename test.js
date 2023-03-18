6a428243db8af9039fc5fd1f6bc549f7eebdf571
Hugo Moroyoqui <hugo_moroyoqui@hotmail.com>
2023-03-17 18:41:09 -0700

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var v1 = 13, v2 =15;
 
 console.log(v1+v2);
 
-const axios = require('axios');
+const axio = require('axios');
 
 const fetchWeatherData = async () => {
     const { data } = await axios.get(
